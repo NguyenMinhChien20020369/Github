fb9d82abf56a444f72e7824f805fbed8a221fd89
Nguyễn Minh Chiến <chien9pm@gmail.com>
2021-04-22 14:34:07 +0700

    7

--- BT07/A/7/main.cpp
+++ BT07/A/7/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    strcmp()
+    string str1= argv[1];
+    string str2= argv[2];
+    if(str1.length()>str2.length())
+    {
+        cout<<0;
+    }
+    else if(str1.length()==str2.length())
+    {
+        if(strcmp(str1.c_str(),str2.c_str())==0)
+        {
+            cout<<1;
+        }
+        else
+        {
+            cout<<0;
+        }
+    }
+    else
+    {
+
+    }
+    //strcmp();
     return 0;
 }
